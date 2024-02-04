f2776ce60566a0e268f15bec2fd355a5bf39b35c
Demochkin Roman <foxheppi319@gmail.com>
2024-02-04 19:46:17 +0600

    Update ReviewsPage

---
+++ js/review.js
@@ -0,0 +1,3 @@
+document.getElementById("sortings").addEventListener('change' , () =>{
+    document.getElementById('button-sort').classList.add('active')
+})
--- php/Reviews.php
+++ php/Reviews.php
@@ -31,20 +31,18 @@ $result = mysqli_query($link, "SELECT * FROM `reviews` ORDER BY $sort $sortinfo"
     <?php
     include ('../pattern/navbar.php')
     ?>
-    <div class="title">
-        <h1>Отзывы</h1>
+        <div class="container-form">
+        <div class="headtitle">
+        <p class="headtitle">Отзывы</p>
     </div>
-    <div class="center">
-    <div class="container">
-        
         <?php
         if (isset($_GET['error'])){ ?>
             <p class="error"><?php echo $_GET['error'] ;?></p>
         <?php } ?>
         <form method="post" action="check/check.php">
 
-            <label for="">Имя: </label><br><input id="name" name="name" placeholder="Имя" type="text"><br><br>
-            <label for="">Коментарий: </label><br><input id="text" name="text" placeholder="Не более 200 символов" type="text"><br><br>
+            <label for="">Введите Имя или Логин:</label><br><input c id="name" name="name" placeholder="Имя" type="text"><br><br>
+            <label for="">Оставте отзыв: </label><br><textarea id="text" name="text" placeholder="Не более 200 символов" type="text"></textarea> <br><br><br>
             <div class="star-rating">
                 <div class="star-rating__wrap">
                   <input class="star-rating__input" id="star-rating-5" type="radio" name="rating" value="5">
@@ -64,24 +62,23 @@ $result = mysqli_query($link, "SELECT * FROM `reviews` ORDER BY $sort $sortinfo"
         </form>
 
         <form method="post" action="check/checksort.php">
-        <select name="sorting" class="sorting">
-                <option hidden selected="true" disabled="disabled">Сортировка</option>
+        <select id="sortings" name="sorting" class="sorting">
+                <option hidden selected disabled="disabled">Сортировка</option>
                 <option value="id">Сортировка по давности</option>
-                <option value="grade">Сортировка по оценки</option>
+                <option value="grade">Сортировка по Оценки</option>
             </select>
-            <input type="submit">
+            <input id="button-sort" value="Сортировать" class="button-sort" type="submit">
         </form>
-        <hr color="gray">
-
+</div><br><br>
+<div class="container">
         <div class="reviews">
         <?php
         while($review = mysqli_fetch_assoc($result)){
             ?>
             <img width="70px" class="photo" src="/vector/usericon.png" alt=""><h2 class="title-review"><?php echo $review['name']?></h2> 
-            <h3>Оценка: <?php  $i=0; while( $i<$review['Grade']){ $i++;?> <svg style="position: relative;
-                top: 7px;" width="30px" height="30px" viewBox="0 0 24 24" fill="#CDA434" xmlns="http://www.w3.org/2000/svg">
+            <h3><?php  $i=0; while( $i<$review['Grade']){ $i++;?> <svg style="float:right;" width="20px" height="20px" viewBox="0 0 24 24" fill="#CDA434" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.15316 5.40838C10.4198 3.13613 11.0531 2 12 2C12.9469 2 13.5802 3.13612 14.8468 5.40837L15.1745 5.99623C15.5345 6.64193 15.7144 6.96479 15.9951 7.17781C16.2757 7.39083 16.6251 7.4699 17.3241 7.62805L17.9605 7.77203C20.4201 8.32856 21.65 8.60682 21.9426 9.54773C22.2352 10.4886 21.3968 11.4691 19.7199 13.4299L19.2861 13.9372C18.8096 14.4944 18.5713 14.773 18.4641 15.1177C18.357 15.4624 18.393 15.8341 18.465 16.5776L18.5306 17.2544C18.7841 19.8706 18.9109 21.1787 18.1449 21.7602C17.3788 22.3417 16.2273 21.8115 13.9243 20.7512L13.3285 20.4768C12.6741 20.1755 12.3469 20.0248 12 20.0248C11.6531 20.0248 11.3259 20.1755 10.6715 20.4768L10.0757 20.7512C7.77268 21.8115 6.62118 22.3417 5.85515 21.7602C5.08912 21.1787 5.21588 19.8706 5.4694 17.2544L5.53498 16.5776C5.60703 15.8341 5.64305 15.4624 5.53586 15.1177C5.42868 14.773 5.19043 14.4944 4.71392 13.9372L4.2801 13.4299C2.60325 11.4691 1.76482 10.4886 2.05742 9.54773C2.35002 8.60682 3.57986 8.32856 6.03954 7.77203L6.67589 7.62805C7.37485 7.4699 7.72433 7.39083 8.00494 7.17781C8.28555 6.96479 8.46553 6.64194 8.82547 5.99623L9.15316 5.40838Z" fill="#CDA434" />
-                </svg><?php  } ?> </h3> <p><?php echo $carbon->diffForHumans($review['date']) ?></p>
+                </svg><?php  } ?> </h3><br> <p style="color: blue;"><?php echo $carbon->diffForHumans($review['date']) ?></p>
             <div class="reviews-block">
             <p class="text-review"><?php echo $review['coment']?></p></div>
             <hr>
@@ -90,7 +87,9 @@ $result = mysqli_query($link, "SELECT * FROM `reviews` ORDER BY $sort $sortinfo"
     ?>
         </div>
     </div>
-</div>
+    <?php
+    include ('../pattern/scrollTop.php')
+    ?>
 <script src="/js/review.js"></script>
 </body>
 </html>
